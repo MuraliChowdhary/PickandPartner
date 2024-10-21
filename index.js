aa8f9e2af5f7cbd4122f40d2c6818e44f398d2bc
Murali <muralisudireddy@gmail.com>
2024-10-21 16:36:15 +0530

    Resolved cors error

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ require("dotenv").config()
 const express = require('express');
 const mongoose = require('mongoose');
 const shortid = require('shortid');
-
+const cors = require("cors")
 // Set up Express
 const app = express();
+
+
+const corsOptions = {
+    origin: 'http://localhost:5175', // Allow only this origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+    credentials: true, // Allow credentials (cookies, authorization headers)
+  };
+ app.use(cors(corsOptions))
 app.use(express.json());
 
 // Connect to MongoDB
